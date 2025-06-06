635c506ad119c7795618131f6bf9e51653224ca5
Craig Hughes <craig.git@rungie.com>
2025-06-06 13:32:45 +0100

    Add Jest support with ts-jest

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^bun:test$': '<rootDir>/test/bun-test.ts'
+    },
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.jest.json' }]
+    }
+};
